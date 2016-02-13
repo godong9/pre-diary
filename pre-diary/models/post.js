2ee59551e6d94bc1ce80b08e50107fd0f28b3eff
Donghyun Go <godong9@gmail.com>
2016-02-13 20:21:01 +0900

    Update posts

--- pre-diary/models/post.js
+++ pre-diary/models/post.js
@@ -7,7 +7,10 @@ var mongoose = require('mongoose'),
  * Post Schema
  */
 var PostSchema = new Schema({
-  author: { type: String, required: true},
+  subject: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: String, required: true },
+  isSecret: { type: Boolean, default: true },
   createDate: { type: Date, default: Date.now() }, // 생성 시간
   updateDate: { type: Date, default: Date.now() } // 수정 시간
 }, { collection: 'posts' });
