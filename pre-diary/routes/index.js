78543bf01fb62ed918517f5584b881f2a9c9ef9e
Donghyun Go <godong9@gmail.com>
2016-02-14 07:22:31 +0900

    Update

--- pre-diary/routes/index.js
+++ pre-diary/routes/index.js
@@ -4,6 +4,8 @@ var express = require('express');
 var router = express.Router();
 var Session = require('../util/session');
 
+var UserModel = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (Session.hasSession(req)) {
@@ -14,10 +16,18 @@ router.get('/', function(req, res, next) {
 
 /* GET User Page. */
 router.get('/user/:id', function(req, res, next) {
+  //TODO: 주석 풀어야함!
+  //if (!Session.hasSession(req)) {
+  //  return res.status(404).send('permission denied');
+  //}
   var viewData = {
     userId: req.session.userId
   };
-  res.render('user', viewData);
+
+  UserModel.findOne({_id: viewData.userId},'-accessToken -refreshToken', function(err, user) {
+    viewData.user = user;
+    res.render('user', viewData);
+  });
 });
 
 /* GET User Emotion Page. */
