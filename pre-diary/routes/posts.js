ce615db2032bc26f797132cca2d7142ca178add8
Donghyun Go <godong9@gmail.com>
2016-02-13 12:41:46 +0900

    Init db

--- pre-diary/routes/posts.js
+++ pre-diary/routes/posts.js
@@ -3,18 +3,16 @@ var router = express.Router();
 var mysql = require('../util/sql').mysql;
 var pool = require('../util/sql').pool;
 
-/* GET users listing. */
+/* GET posts listing. */
 router.get('/', function(req, res, next) {
 
-  pool.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-
-    console.log('The solution is: ', rows[0].solution);
-    res.send('respond with a resource');
-  });
 });
 
 router.get('/:id', function(req, res, next) {
+
+});
+
+router.post('/', function(req, res, next) {
 
 });
 
