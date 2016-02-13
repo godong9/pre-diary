e1ab13ca126bf00807894dd27ef0d0fb0c2e69f5
Donghyun Go <godong9@gmail.com>
2016-02-13 21:54:15 +0900

    Update doc

--- pre-diary/doc/api_project.js
+++ pre-diary/doc/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "pre-diary",  "version": "0.0.1",  "description": "Pre Diary",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-02-13T05:21:09.491Z",    "url": "http://apidocjs.com",    "version": "0.15.0"  }});
+define({  "name": "pre-diary",  "version": "0.0.1",  "description": "Pre Diary",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-02-13T12:54:11.493Z",    "url": "http://apidocjs.com",    "version": "0.15.0"  }});
