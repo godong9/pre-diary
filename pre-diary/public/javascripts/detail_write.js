e8a0e1cca8e6c8f7fb5de3c86977f644ea2b4fb8
Donghyun Go <godong9@gmail.com>
2016-02-14 09:17:05 +0900

    D

--- pre-diary/public/javascripts/detail_write.js
+++ pre-diary/public/javascripts/detail_write.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	//detail- page	
 		jQuery.ajax({
    	   type:"GET",
-			 url:"//godong9.com:3001/posts/"+$('user_main').data('post'),
+			 url:"//godong9.com:3001/posts/"+$('#user_main').data('post'),
 			 dataType:"JSON", // 옵션이므로 JSON으로 받을게 아니면 안써도 됨
 			 success : function(data) {
 			 	console.log(data);
